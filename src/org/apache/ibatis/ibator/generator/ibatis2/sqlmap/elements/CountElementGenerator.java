0e393aca1a2303a98662d5e7b54c3da422bbc112
fanwt7236 <fanwt7236@163.com>
2017-02-13 17:47:17 +0800

    扩展的一些方法

---
+++ src/org/apache/ibatis/ibator/generator/ibatis2/sqlmap/elements/CountElementGenerator.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2008 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.ibatis.ibator.generator.ibatis2.sqlmap.elements;
+
+import org.apache.ibatis.ibator.api.FullyQualifiedTable;
+import org.apache.ibatis.ibator.api.IntrospectedColumn;
+import org.apache.ibatis.ibator.api.dom.java.FullyQualifiedJavaType;
+import org.apache.ibatis.ibator.api.dom.xml.Attribute;
+import org.apache.ibatis.ibator.api.dom.xml.TextElement;
+import org.apache.ibatis.ibator.api.dom.xml.XmlElement;
+
+/**
+ * 
+ * @author fanwt7236@163.com
+ *
+ */
+public class CountElementGenerator extends
+        AbstractXmlElementGenerator {
+
+    public CountElementGenerator() {
+        super();
+    }
+
+    @Override
+    public void addElements(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("select"); //$NON-NLS-1$
+        FullyQualifiedTable table = introspectedTable.getFullyQualifiedTable();
+
+        answer.addAttribute(new Attribute(
+                "id", "count")); //$NON-NLS-1$
+
+        FullyQualifiedJavaType parameterType;
+        
+        if (introspectedTable.getRules().generateRecordWithBLOBsClass()) {
+            parameterType = introspectedTable.getRecordWithBLOBsType();
+        } else {
+            parameterType = introspectedTable.getBaseRecordType();
+        }
+        
+        answer.addAttribute(new Attribute("parameterClass", //$NON-NLS-1$
+                parameterType.getFullyQualifiedName()));
+        
+        answer.addAttribute(new Attribute(
+                "resultClass", "java.lang.Integer")); 
+        
+        ibatorContext.getCommentGenerator().addComment(answer);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("select count(*) from "); //$NON-NLS-1$
+        sb.append(table.getFullyQualifiedTableNameAtRuntime());
+        answer.addElement(new TextElement(sb.toString()));
+
+        XmlElement dynamicElement = new XmlElement("dynamic"); //$NON-NLS-1$
+        dynamicElement.addAttribute(new Attribute("prepend", "where")); //$NON-NLS-1$ //$NON-NLS-2$
+        answer.addElement(dynamicElement);
+
+        for (IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+            XmlElement isNotNullElement = new XmlElement("isNotNull"); //$NON-NLS-1$
+            isNotNullElement.addAttribute(new Attribute("prepend", "and")); //$NON-NLS-1$ //$NON-NLS-2$
+            isNotNullElement.addAttribute(new Attribute("property", introspectedColumn.getJavaProperty())); //$NON-NLS-1$
+            dynamicElement.addElement(isNotNullElement);
+
+            sb.setLength(0);
+            sb.append(introspectedColumn.getEscapedColumnName());
+            sb.append(" = "); //$NON-NLS-1$
+            sb.append(introspectedColumn.getIbatisFormattedParameterClause());
+            
+            isNotNullElement.addElement(new TextElement(sb.toString()));
+        }
+
+        if (ibatorContext.getPlugins().sqlMapCountGenerated(answer, introspectedTable)) {
+            parentElement.addElement(answer);
+        }
+    }
+}
---
+++ src/org/apache/ibatis/ibator/generator/ibatis2/sqlmap/elements/SelectListElementGenerator.java
@@ -0,0 +1,107 @@
+/*
+ *  Copyright 2008 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.ibatis.ibator.generator.ibatis2.sqlmap.elements;
+
+import org.apache.ibatis.ibator.api.FullyQualifiedTable;
+import org.apache.ibatis.ibator.api.IntrospectedColumn;
+import org.apache.ibatis.ibator.api.dom.xml.Attribute;
+import org.apache.ibatis.ibator.api.dom.xml.TextElement;
+import org.apache.ibatis.ibator.api.dom.xml.XmlElement;
+import org.apache.ibatis.ibator.generator.ibatis2.XmlConstants;
+import org.apache.ibatis.ibator.internal.util.StringUtility;
+
+/**
+ * 
+ * @author fanwt7236@163.com
+ *
+ */
+public class SelectListElementGenerator extends AbstractXmlElementGenerator {
+
+    public SelectListElementGenerator() {
+        super();
+    }
+
+    @Override
+    public void addElements(XmlElement parentElement) {
+        XmlElement answer = new XmlElement("select"); //$NON-NLS-1$
+        FullyQualifiedTable table = introspectedTable.getFullyQualifiedTable();
+
+        answer.addAttribute(new Attribute(
+                "id", "selectList")); //$NON-NLS-1$
+        if (introspectedTable.getRules().generateResultMapWithBLOBs()) {
+            answer.addAttribute(new Attribute("resultMap", //$NON-NLS-1$
+                    XmlConstants.RESULT_MAP_WITH_BLOBS_ID));
+        } else {
+            answer.addAttribute(new Attribute("resultMap", //$NON-NLS-1$
+                    XmlConstants.BASE_RESULT_MAP_ID));
+        }
+        
+        answer.addAttribute(new Attribute("parameterClass", //$NON-NLS-1$
+                introspectedTable.getBaseRecordType().getFullyQualifiedName()));
+
+        ibatorContext.getCommentGenerator().addComment(answer);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("select "); //$NON-NLS-1$
+
+        boolean comma = false;
+        if (StringUtility.stringHasValue(introspectedTable.getSelectByPrimaryKeyQueryId())) {
+            sb.append('\'');
+            sb.append(introspectedTable.getSelectByPrimaryKeyQueryId());
+            sb.append("' as QUERYID"); //$NON-NLS-1$
+            comma = true;
+        }
+
+        for (IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+            if (comma) {
+                sb.append(", "); //$NON-NLS-1$
+            } else {
+                comma = true;
+            }
+
+            sb.append(introspectedColumn.getSelectListPhrase());
+        }
+
+        answer.addElement(new TextElement(sb.toString()));
+
+        sb.setLength(0);
+        sb.append("from "); //$NON-NLS-1$
+        sb.append(table.getAliasedFullyQualifiedTableNameAtRuntime());
+        answer.addElement(new TextElement(sb.toString()));
+
+        XmlElement dynamicElement = new XmlElement("dynamic"); //$NON-NLS-1$
+        dynamicElement.addAttribute(new Attribute("prepend", "where")); //$NON-NLS-1$ //$NON-NLS-2$
+        answer.addElement(dynamicElement);
+
+        for (IntrospectedColumn introspectedColumn : introspectedTable.getAllColumns()) {
+            XmlElement isNotNullElement = new XmlElement("isNotNull"); //$NON-NLS-1$
+            isNotNullElement.addAttribute(new Attribute("prepend", "and")); //$NON-NLS-1$ //$NON-NLS-2$
+            isNotNullElement.addAttribute(new Attribute("property", introspectedColumn.getJavaProperty())); //$NON-NLS-1$
+            dynamicElement.addElement(isNotNullElement);
+
+            sb.setLength(0);
+            sb.append(introspectedColumn.getEscapedColumnName());
+            sb.append(" = "); //$NON-NLS-1$
+            sb.append(introspectedColumn.getIbatisFormattedParameterClause());
+            
+            isNotNullElement.addElement(new TextElement(sb.toString()));
+        }
+
+        if (ibatorContext.getPlugins().sqlMapSelectOneGenerated(answer, introspectedTable)) {
+            parentElement.addElement(answer);
+        }
+    }
+}
